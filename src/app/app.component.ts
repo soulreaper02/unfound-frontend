7851943a9af6d1d520ca642e7e47e231f3083faa
Amit Kumar <Soulreaper@Amits-MacBook-Pro.local>
2018-10-20 12:46:41 +0530

    code push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'unfound-angular';
+  title = 'unfound';
+
+  tabledata : [{
+    vehicleid : string,
+    frame : string,
+    powertrain : string,
+    wheelcount : string,
+    vehicletype : string,
+    upload : string
+  }]
+
+  tabledata2 : [{
+    vehicleid : string,
+    frame : string,
+    powertrain : string,
+    wheelcount : string,
+    vehicletype : string,
+    upload : string
+  }]
+
+  filedata : [{
+    filename : String,
+    filepath : String
+  }]
+  
+
+  selectedFile: File = null;
+  constructor(private http: HttpClient) { }
+  onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+    const regex = new RegExp("(.*?)\.(xml)$")
+    if (regex.test(this.selectedFile.name)) {
+      const formData = new FormData();
+      formData.append('upload', this.selectedFile, this.selectedFile.name)
+      this.http.post('http://localhost:9000/fileupload', formData, {
+        responseType: 'text',
+        reportProgress: true,
+        observe: 'events'
+      }).subscribe(res => {
+        if (res.type === HttpEventType.UploadProgress) {
+          let uploadProgress = Math.round(res.loaded / res.total * 100 ) + '%';
+          console.log('Upload Progress: ', uploadProgress);
+        } else if (res.type === HttpEventType.Response) {
+          let result = JSON.parse(res.body);
+          if (result) {
+            this.http.post('http://localhost:9000/process', result, {
+              responseType: 'text'
+            }).subscribe( res => {
+              let data = JSON.parse(res);
+              console.log(data);
+              this.tabledata = data;
+            })
+          }
+        }
+      })
+    } else {
+      alert('Unsupported file type !!!');
+    }
+  }
+
+  checkTab(event) {
+    if (event.index == '1') {
+      this.http.get('http://localhost:9000/getallfiles', {
+        responseType : 'text',
+      }).subscribe( res => {
+        let data = JSON.parse(res);
+        console.log(data);
+        this.filedata = data;
+      })
+    }
+  }
+
+  LinkToFile(filepath: string) {
+    console.log(filepath);
+    window.open(filepath);
+  }
+
+  getReports(filename : string) {
+    console.log(filename);
+    const formData = new FormData();
+    formData.append('filename', filename)
+    this.http.post('http://localhost:9000/getreports', formData, {
+      responseType : 'text',
+    }).subscribe (res => {
+      let data = JSON.parse(res);
+      console.log(data);
+      this.tabledata2 = data;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
